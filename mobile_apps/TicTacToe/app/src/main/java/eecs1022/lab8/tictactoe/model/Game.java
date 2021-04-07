a6597327925879b06aba2200166f84217c68fd24
adnan500 <adnan500@my.yorku.ca>
2021-04-06 21:49:48 -0400

    Update Lab8

--- mobile_apps/TicTacToe/app/src/main/java/eecs1022/lab8/tictactoe/model/Game.java
+++ mobile_apps/TicTacToe/app/src/main/java/eecs1022/lab8/tictactoe/model/Game.java
@@ -1,199 +1,242 @@
 package eecs1022.lab8.tictactoe.model;
 
+import android.provider.Settings;
+
 public class Game {
     // Attributes
     private String playerX;
     private String playerO;
-    private char currentPlayer;
-    private static char[][] board = {{'_', '_', '_'}, {'_', '_', '_'}, {'_', '_', '_'}};
+    private boolean winnerExist;
+    private int changeTurn;
 
-    private int row;
-    private int column;
+    private char winner;
+    private String winnerName;
 
-    private boolean errorExist;
+    private  boolean errorExist;
     private String errorMsg;
-    private boolean specialCase;
+    private int attempt;
 
-    private int turnInt;
-
+    private static char[][] board = {
+            {'_', '_', '_'},
+            {'_', '_', '_'},
+            {'_', '_', '_'}
+    };
     // Constructor
     public Game(String playerX, String playerO) {
-        this.playerX = playerX;
         this.playerO = playerO;
-        this.currentPlayer = 'x';
-        Game.board = new char[][]{{'_', '_', '_'}, {'_', '_', '_'}, {'_', '_', '_'}};
+        this.playerX = playerX;
+        Game.board = new char[][]{
+                {'_', '_', '_'},
+                {'_', '_', '_'},
+                {'_', '_', '_'}
+        };
 
-        this.row = 0;
-        this.column = 0;
-
-        this.errorExist = false;
     }
 
     // Accessor
     public String getCurrentPlayer() {
-        String result = "";
-        boolean contin = this.Continue(Game.board);
-        if (!contin) {
-            if (!specialCase) {
-                if (this.turnInt % 2 == 0) {
-                    result = this.playerX;
-                }
-                else if (this.turnInt % 2 == 1) {
-                    result = this.playerO;
-                }
-            }
-            else {
-                if (this.turnInt != 1 && this.turnInt % 2 == 1) {
-                    result = this.playerX;
-                }
-                else if (this.turnInt == 1 ) {
-                    result = this.playerO;
-                }
-                else if (this.turnInt % 2 == 0) {
-                    result = this.playerO;
-                }
-            }
-        }
-        else {
-            result = null;
-        }
+        String result = null;
+        this.CheckForTheWinner();
 
+        if (!this.winnerExist) {
+            if (this.changeTurn % 2 == 0) {
+                result = this.playerX;
+            }
+            else if (this.changeTurn % 2 == 1) {
+                result = this.playerO;
+            }
+        }
 
         return  result;
     }
 
     public String getStatus() {
-        String result = "";
+        String result = this.errorMsg;
+        this.CheckForTheWinner();
 
-        if (errorExist) {
-            result = this.errorMsg;
-        }
-        else {
-            if(!specialCase) {
-                if (this.turnInt % 2 == 0) {
-                    result = this.playerX + "'s turn to play...";;
+
+        if (!this.winnerExist) {
+            if (!this.errorExist) {
+                if (this.changeTurn % 2 == 0) {
+                    result = this.playerX + "'s turn to play...";
                 }
-                else if (this.turnInt % 2 == 1) {
-                    result = this.playerO + "'s turn to play...";;
-                }
-            }
-            else {
-                if (this.turnInt != 1 && this.turnInt % 2 == 1) {
-                    result = this.playerX + "'s turn to play...";;
-                }
-                else if (this.turnInt == 1) {
-                    result = this.playerO + "'s turn to play...";;
-                }
-                else if (this.turnInt == 0) {
-                    result = this.playerO + "'s turn to play...";;
+                else if (this.changeTurn % 2 == 1) {
+                    result = this.playerO + "'s turn to play...";
                 }
             }
         }
+        else if (this.attempt == 0) {
+            result = "Game is over with " + this.winnerName(this.winner) + " being the winner.";
+            this.attempt ++;
+        }
+
 
         return  result;
     }
 
     public char[][] getBoard(){
-        if (!specialCase) {
-            if (!errorExist && this.turnInt != 0 ) {
-                if (this.turnInt % 2 == 1)
-                    Game.board[this.row - 1][this.column - 1] = 'x';
-                else if (this.turnInt % 2 == 0) {
-                    Game.board[this.row - 1][this.column - 1] = 'o';
-                }
-            }
-        }
-        else {
-            if (!errorExist && this.turnInt != 1) {
-                if (this.turnInt % 2 == 1)
-                    Game.board[this.row - 1][this.column - 1] = 'x';
-                else if (this.turnInt % 2 == 0) {
-                    Game.board[this.row - 1][this.column - 1] = 'o';
-                }
-            }
-        }
-
         return Game.board;
+
     }
 
     // Mutator
     public void setWhoPlaysFirst(char letter) {
         if (letter == 'o') {
-            this.turnInt = 1;
-            this.specialCase = true;
+            this.changeTurn = 1;
         }
-        this.currentPlayer = letter;
+        else if (letter == 'x') {
+            this.changeTurn = 0;
+        }
     }
 
     public void move(int row, int column) {
         this.errorExist = false;
-        boolean contin = this.Continue(Game.board);
+        this.CheckForTheWinner();
 
-        if (!contin) {
-            if ((row >= 1 && row <= 3) && (column >= 1 && column <= 3)) {
-                if (Game.board[row - 1][column - 1] == 'x' || Game.board[row - 1][column - 1] == 'o') {
+        if (this.winnerExist) {
+            this.errorExist = true;
+            this.errorMsg = "Error: game is already over with a winner.";
+        }
+        else {
+            if (row < 1 || row > 3) {
+                this.errorExist = true;
+                this.errorMsg = "Error: row " + row + " is invalid.";
+            }
+            else if (column < 1 || column > 3) {
+                this.errorExist = true;
+                this.errorMsg = "Error: col " + column + " is invalid.";
+            }
+            else {
+                if (Game.board[row - 1][column - 1] != '_') {
                     this.errorExist = true;
                     this.errorMsg = "Error: slot @ (" + row + ", " + column + ") is already occupied.";
                 }
                 else {
-                    this.row = row;
-                    this.column = column;
-                    this.turnInt++;
+                    if (this.changeTurn % 2 == 0) {
+                        Game.board[row - 1][column - 1] = 'x';
+                    }
+                    else if (this.changeTurn % 2 == 1) {
+                        Game.board[row - 1][column - 1] = 'o';
+                    }
+                    this.changeTurn ++;
                 }
             }
-            else if  (row < 1 || row > 3){
-                this.errorExist = true;
-                this.errorMsg = "Error: row " + row + " is invalid.";
-            }
-            else if  (column < 1 || column > 3){
-                this.errorExist = true;
-                this.errorMsg = "Error: col " + column + " is invalid.";
-            }
-        }
-        else  {
-            this.errorExist = true;
-            this.errorMsg = "Game is over with Suyeon being the winner.";
         }
 
     }
 
     // Helper Method
-    public boolean Continue(char[][] a) {
-        boolean winner = false;
+    public void CheckForTheWinner() {
         boolean stay = true;
 
-        for (int i = 0; stay && i < a.length; i ++) {
-            if (a[i][i] == 'x') {
-                winner = true;
+        // -m Diagonal
+        for(int i = 0; stay && i < 3; i ++) {
+            if (Game.board[i][i] == 'x') {
+                this.winnerExist = true;
+                this.winner = 'x';
             }
             else {
-                winner = false;
+                this.winnerExist = false;
                 stay = false;
             }
         }
-        if (!winner) {
+
+        if (!this.winnerExist) {
             stay = true;
-            for (int i = 0; stay && i < a.length; i ++) {
-                if (a[i][i] == 'o') {
-                    winner = true;
+            for(int i = 0; stay && i < 3; i ++) {
+                if (Game.board[i][i] == 'o') {
+                    this.winnerExist = true;
+                    this.winner = 'o';
                 }
                 else {
-                    winner = false;
+                    this.winnerExist = false;
                     stay = false;
                 }
             }
         }
 
+    // --------------------------------------------------
 
-//        for (int i = 0; i < a.length; i ++) {
-//            char firstChar = 'x';
-//            for (int m = 0; m < a[i].length; m ++) {
-//                if ()
-//            }
-//        }
-        return winner;
+        // +m  Diagonal
+        for(int i = 0; stay && i < 3; i ++) {
+            if (Game.board[i][2 - i] == 'x') {
+                this.winnerExist = true;
+                this.winner = 'x';
+            }
+            else {
+                this.winnerExist = false;
+                stay = false;
+            }
+        }
+
+        if (!this.winnerExist) {
+            stay = true;
+            for(int i = 0; stay && i < 3; i ++) {
+                if (Game.board[i][2 - i] == 'o') {
+                    this.winnerExist = true;
+                    this.winner = 'o';
+                }
+                else {
+                    this.winnerExist = false;
+                    stay = false;
+                }
+            }
+        }
+
+        // -----------------------------------------------
+
+        // Row checking
+        if (!this.winnerExist) {
+            stay = true;
+            for (int i = 0; i < 3; i ++) {
+                for (int m = 0; stay && m < 3; m ++) {
+                    if (Game.board[i][m] == 'x') {
+                        this.winnerExist = true;
+                        this.winner = 'x';
+                    }
+                    else {
+                        this.winnerExist = false;
+                        stay = false;
+                    }
+                }
+            }
+        }
+
+        if (!this.winnerExist) {
+            stay = true;
+            for (int i = 0; i < 3; i ++) {
+                for (int m = 0; stay && m < 3; m ++) {
+                    if (Game.board[i][m] == 'o') {
+                        this.winnerExist = true;
+                        this.winner = 'o';
+                    }
+                    else {
+                        this.winnerExist = false;
+                        stay = false;
+                    }
+                }
+            }
+        }
+
+        // -----------------------------------------------------
+
+        
+
+
+
+
+
     }
 
+    public String winnerName(char c) {
+        if (c == 'x') {
+            this.winnerName = this.playerX;
+        }
+        else if (c == 'o'){
+            this.winnerName = this.playerO;
+        }
+        return this.winnerName;
+    }
 }
 
 
